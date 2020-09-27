79e623a6120219e6ad54d2706e105e9f69472fbd
watersmark <DuldinEV@mail.ru>
2020-09-27 19:07:27 +0300

    FileInputStream

--- InputOutputStream/src/InputOutput.java
+++ InputOutputStream/src/InputOutput.java
@@ -1,22 +1,35 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class InputOutput {
     public static void main(String[] args) throws IOException {
+        FileInputStream file = null;
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            int i;
+            // Открываем файл
+            file = new FileInputStream("TestLecture1.txt");
+            do {
+                i = file.read();
+                System.out.print((char) i);
+                //System.out.print(" ");
+            } while (i != -1);
 
-        while(true){
-            String elem = reader.readLine();
-
-            if(elem.equals("a")){
-                break;
-            }
-
-            System.out.println(elem);
         }
-
+        catch (IOException e){
+            System.out.println("Io exception");
+        }
+        finally {
+            // Пытаемся закрыть файл
+            try {
+                file.close();
+            }
+            catch (NullPointerException w){
+                System.out.println("Error close file");
+            }
+        }
 
     }
 }
